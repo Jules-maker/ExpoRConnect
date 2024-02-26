e839f17d0f9362703482af47ccfe884463130475
loulou761001 <louis.cherik@gmail.com>
2024-02-26 11:12:06 +0100

    restart project

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
-/// <reference types="nativewind/types" />
+import React from "react";
 import { StatusBar } from 'expo-status-bar';
 import {  Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+        <Text style={{backgroundColor:"blue"}}>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
     </View>
   );
 }

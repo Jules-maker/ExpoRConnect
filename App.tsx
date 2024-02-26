79616a2aeb99d60af8897e99886da19716853530
loulou761001 <louis.cherik@gmail.com>
2024-02-26 11:16:55 +0100

    :sparkles: add prettier

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { StatusBar } from 'expo-status-bar';
-import {  Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
     <View>
-        <Text style={{backgroundColor:"blue"}}>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style="auto" />
+      <Text style={{ backgroundColor: "blue" }}>
+        Open up App.tsx to start working on your app!
+      </Text>
+      <StatusBar style="auto" />
     </View>
   );
 }

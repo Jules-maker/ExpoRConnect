8f7695a9207c12d55e5f9adc617314aa75088db9
Johann six <six.johann@gmail.com>
2024-02-27 11:25:48 +0100

    ✏️ Rename to host Screnn

--- app/(tabs)/host/index.tsx
+++ app/(tabs)/host/index.tsx
@@ -2,7 +2,7 @@ import { StyleSheet } from 'react-native';
 
 import { Text, View } from '@/components/Themed';
 
-export default function TabTwoScreen() {
+export default function HostScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Liste</Text>

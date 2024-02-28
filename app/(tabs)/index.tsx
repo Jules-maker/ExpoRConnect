a7a287089026deadd619853669be3ad223bab032
selim27000 <selim27000@hotmail.com>
2024-02-28 17:55:12 +0100

    add mobile slider component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,18 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
+import { StyleSheet, Image, useColorScheme } from 'react-native';
 import { Text, View } from '@/components/Themed';
+import CarouselAutoScroll from '../../components/Carousel/Slider';
 
 export default function TabOneScreen() {
+  const colorScheme = useColorScheme();
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <View style={styles.logoContainer}>
+        <Image source={require('../../assets/images/logo.png')} style={styles.logo} />
+      </View>
+      <CarouselAutoScroll />
+      <Text style={styles.title}>Inscription</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   );
 }
@@ -17,7 +21,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+  },
+  logoContainer: {
+    marginTop: 0,
+  },
+  logo: {
+    width: 250,
+    height: 250,
   },
   title: {
     fontSize: 20,
---
+++ components/Carousel/SlideItem.tsx
@@ -0,0 +1,83 @@
+import React, { useEffect } from 'react';
+import { Image, StyleSheet, Text, View, Dimensions, Animated, Easing } from 'react-native';
+
+const { width, height } = Dimensions.get('screen');
+
+interface SlideItemProps {
+  item: {
+    img: any; // Type de données pour img
+    title: string;
+    description: string;
+    price: string;
+  };
+}
+
+const SlideItem: React.FC<SlideItemProps> = ({ item }) => {
+  const translateYImage = new Animated.Value(40);
+
+  useEffect(() => {
+    Animated.timing(translateYImage, {
+      toValue: 0,
+      duration: 1000,
+      useNativeDriver: true,
+      easing: Easing.bounce,
+    }).start();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Animated.Image
+        source={item.img}
+        resizeMode="contain"
+        style={[
+          styles.image,
+          {
+            transform: [
+              {
+                translateY: translateYImage,
+              },
+            ],
+          },
+        ]}
+      />
+
+      <View style={styles.content}>
+        <Text style={styles.title}>{item.title}</Text>
+        <Text style={styles.description}>{item.description}</Text>
+        <Text style={styles.price}>{item.price}</Text>
+      </View>
+    </View>
+  );
+};
+
+export default SlideItem;
+
+const styles = StyleSheet.create({
+  container: {
+    width,
+    height,
+    alignItems: 'center',
+  },
+  image: {
+    flex: 0.6,
+    width: '100%',
+  },
+  content: {
+    flex: 0.4,
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  description: {
+    fontSize: 18,
+    marginVertical: 12,
+    color: '#333',
+  },
+  price: {
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+});

b8723b7a9461997b6e37d46b1309b3ae58589126
selim27000 <selim27000@hotmail.com>
2024-02-29 03:44:16 +0100

    add homeview to start

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,8 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
+import HomePage from './home';
+
 
 
 import { useColorScheme } from '@/components/useColorScheme';
@@ -15,7 +17,7 @@ export {
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: 'home',
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -51,9 +53,10 @@ function RootLayoutNav() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
+        <Stack.Screen name="home" options={{ headerShown: true }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
       </Stack>
     </ThemeProvider>
-  );
+      );
 }
---
+++ app/home.tsx
@@ -0,0 +1,42 @@
+import { StyleSheet, Image, useColorScheme } from 'react-native';
+import { Text, View } from '@/components/Themed';
+import CarouselAutoScroll from '../components/Carousel/Slider';
+
+export default function HomePage() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.logoContainer}>
+        <Image source={require('../assets/images/logo.png')} style={styles.logo} />
+      </View>
+      <CarouselAutoScroll />
+      <Text style={styles.title}>Inscription</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  logoContainer: {
+    marginTop: 0,
+  },
+  logo: {
+    width: 250,
+    height: 250,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});

a35852bec557255540bbb7d1507640d0f36fe64d
Johann six <six.johann@gmail.com>
2024-02-29 11:14:16 +0100

    🔥 Remove logout button from MapScreen

--- app/(tabs)/map/index.tsx
+++ app/(tabs)/map/index.tsx
@@ -1,21 +1,14 @@
-import { Button, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
-import * as SecureStore from 'expo-secure-store';
-import { router } from 'expo-router';
 
 export default function MapScreen() {
-  const handleLogout = async () => {
-    await SecureStore.deleteItemAsync('jwtToken');
-    router.navigate('/begining');
-  };
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Map</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/map/index.tsx" />
-      <Button title="logout" onPress={handleLogout} />
     </View>
   );
 }

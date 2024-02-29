4814ee98c5e19d77c36a1e7fd7a752380847b54a
Johann six <six.johann@gmail.com>
2024-02-29 17:02:37 +0100

    ♻️ Refactor HomeScreen component to fetch data from API

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,30 +3,42 @@ import { StyleSheet } from 'react-native';
 import { View } from '@/components/Themed';
 import ListComponent from '@/components/ListComponent';
 import Cards, { CardsProps } from '@/components/CardComponent';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Stack } from 'expo-router';
+import { useSession } from '@/components/Ctx';
+import { api } from '@/tools/Api';
+import { Host } from '@/models/hostModel';
 
 export default function HomeScreen() {
-  const cardsList = Array.from({length: 10}, createFalseData);
-  const [list, setList] = useState<CardsProps[]>(cardsList);
-  const handleData = () => {
-    console.info('adding new data');
-    const randomTitle = Math.random().toString(36).substring(7);
-    const randomImg = `https://source.unsplash.com/random/320x320?sig=${Math.random()}`;
+  const { session } = useSession();
 
-    const newCard = {
-      name: randomTitle,
-      id: Math.random().toString(36).substring(7),
-      description: 'lorem ipsum',
-      mainphoto: randomImg,
-    };
-    setList([...list, newCard]);
-    console.info('new data added');
+  const [list, setList] = useState<Host[]>([]);
+  const [page, setPage] = useState(1);
+  const [totalCount, setTotalCount] = useState(1);
+
+  const handleData =async  () => {
+    console.info("adding new data");
+    try {
+      const { data } = await api.get(
+        `api/Host?page=${page}`,
+        { headers: { Authorization: session } },
+      );
+      console.log(data);
+      setList(data.data);
+      setTotalCount(data.totalCount);
+      setPage(page + 1);
+    } catch (e) {
+      console.log("erreur", e);
+    }
   }
+
+  useEffect(() => {
+    handleData();
+  }, []);
   return (
     <View style={styles.container}>
       <Stack.Screen options={{ headerShown: true, title: `Accueil` }} />
-      <ListComponent  items={list} renderItem={(item) => <Cards {...item} />} triggerRefresh={handleData} nbColumns={2}/>
+      <ListComponent  items={list} renderItem={(item) => <Cards {...item} />} triggerRefresh={list.length < totalCount ? handleData : () => null} nbColumns={2}/>
     </View>
   );
 }
@@ -47,15 +59,15 @@ const styles = StyleSheet.create({
   },
 });
 
-const createFalseData = (): CardsProps => {
-  const randomTitle = Math.random().toString(36).substring(7);
-  const randomImg = `https://source.unsplash.com/random/320x320?sig=${Math.random()}`;
+// const createFalseData = (): CardsProps => {
+//   const randomTitle = Math.random().toString(36).substring(7);
+//   const randomImg = `https://source.unsplash.com/random/320x320?sig=${Math.random()}`;
 
-  const newCard = {
-    name: randomTitle,
-    id: Math.random().toString(36).substring(7),
-    description: 'lorem ipsum',
-    mainphoto: randomImg,
-  }
-  return newCard;
-}
+//   const newCard = {
+//     name: randomTitle,
+//     id: Math.random().toString(36).substring(7),
+//     description: 'lorem ipsum',
+//     mainphoto: randomImg,
+//   }
+//   return newCard;
+// }

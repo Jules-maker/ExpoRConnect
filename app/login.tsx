1fe7c67f26c9d34961cefa0eeab7a4e8472d14dd
Johann six <six.johann@gmail.com>
2024-03-01 10:06:59 +0100

    🐛 Add Stack.Screen component to LoginScreen to remove header

--- app/login.tsx
+++ app/login.tsx
@@ -2,10 +2,12 @@ import { StyleSheet } from 'react-native';
 
 import { Text, View } from '@/components/Themed';
 import LoginFormComponent from '@/components/LoginFormComponent';
+import { Stack } from 'expo-router';
 
 export default function LoginScreen() {
   return (
     <View style={styles.container}>
+      <Stack.Screen options={{ title: 'Login', headerBackVisible: false }} />
       <Text style={styles.title}>Login</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <LoginFormComponent />

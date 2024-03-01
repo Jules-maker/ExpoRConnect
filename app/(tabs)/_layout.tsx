234fad3e00a0d9f4c5ac3d5fbcdddf20383c3fda
Johann six <six.johann@gmail.com>
2024-03-01 15:08:46 +0100

    🎨 Add Link and Pressable components to TabLayout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Redirect, Tabs } from 'expo-router';
-import { ActivityIndicator } from 'react-native';
+import { Link, Redirect, Tabs } from 'expo-router';
+import { ActivityIndicator, Pressable } from 'react-native';
 
 import Colors from '@/constants/Colors';
 import { useColorScheme } from '@/components/useColorScheme';
-import { useClientOnlyValue } from '@/components/useClientOnlyValue';
 import { useSession } from "@/components/Ctx";
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
@@ -39,11 +38,27 @@ export default function TabLayout() {
         headerTitleAlign: 'center',
       }}>
       <Tabs.Screen
-        name="home"
+        name="index"
         options={{
-          href: '/home',
+          headerShown: true,
+          href: '/',
           title: 'Accueil',
           tabBarIcon: ({ color }) => <TabBarIcon name="feed" color={color} />,
+            headerRight: () => (
+                <Link href="/modal" asChild>
+                    <Pressable>
+                        {({ pressed }) => (
+                            <FontAwesome
+                                name="info-circle"
+                                size={25}
+                                color={Colors[colorScheme ?? 'light'].text}
+                                style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                            />
+                        )}
+                    </Pressable>
+                </Link>
+            ),
+       
         }}
       />
       <Tabs.Screen

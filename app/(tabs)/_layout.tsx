3f5cd0dc1c179535f8ac70a6f30bfca556088083
Johann six <six.johann@gmail.com>
2024-03-18 10:52:45 +0100

    ✨ Add page events

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -62,6 +62,14 @@ export default function TabLayout() {
        
         }}
       />
+      <Tabs.Screen 
+        name="events"
+        options={{
+          href: '/events',
+          title: 'Notifications',
+          tabBarIcon: ({ color }) => <TabBarIcon name="bell" color={color} />,
+        }}
+      />
       <Tabs.Screen
         name="map"
         options={{
